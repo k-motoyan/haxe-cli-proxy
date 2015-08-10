8ee36171b226b9c729d0d79c9e592db1df756c30
k-motoyan <k.motoyan888@gmail.com>
2015-08-10 20:01:12 +0900

    add codeclimate coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,8 @@ SimpleCov.start do
   add_filter '/spec/'
 end
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'haxe_cli_proxy'
